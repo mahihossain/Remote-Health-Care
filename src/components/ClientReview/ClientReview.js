28d820273a4002724e4bcc957f13bd208060c4fd
mahihossain <sayed.muddashir@gmail.com>
2019-10-07 13:16:53 +0600

    created ClientReview component

---
+++ src/components/ClientReview/ClientReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ClientReview.module.css';
+
+const ClientReview = () => {
+    return (
+        <div className={styles.ClientReview}>
+            <h1>Hi I am review!!</h1>
+        </div>
+    );
+}
+
+export default ClientReview;
