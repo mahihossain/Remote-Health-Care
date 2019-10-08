e5077fb27f880035f1c0afcf966444e85926bbc9
mahihossain <sayed.muddashir@gmail.com>
2019-10-08 17:10:22 +0600

    Added the carousel

--- src/components/ClientReview/ClientReview.js
+++ src/components/ClientReview/ClientReview.js
@@ -1,161 +1,65 @@
-import React, { Fragment } from 'react'
-import styles from './ClientReview.module.css'
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
+import React, { Fragment } from "react";
+import styles from "./ClientReview.module.css";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
-const ClientReview = () => {
-	return (
-		<ParallaxProvider>
-			<Fragment>
-				<div className={styles.ClientReview}>
-					<Parallax
-						className="parallax"
-						y={[-30, 30]}
-						tagOuter="figure">
-						<h1>Review</h1>
-						<p>
-							Ipsum sint quis aliquip quis sunt aliqua nulla
-							pariatur laboris. Ut laboris ipsum sit et magna et
-							in ea. Fugiat nostrud adipisicing reprehenderit amet
-							esse veniam dolor qui ullamco anim. Officia ad in
-							minim aliqua magna sint ipsum consectetur
-							adipisicing cillum. Laboris velit irure ex ad
-							laboris. Ea aliquip ipsum veniam eiusmod eu ea et
-							dolor cillum officia cillum eiusmod. Esse nostrud
-							incididunt nostrud cillum excepteur tempor amet
-							labore anim consectetur Lorem. Et laborum quis
-							eiusmod sit dolor pariatur amet ad deserunt ullamco
-							ex. Ullamco incididunt Lorem nisi irure. Culpa aute
-							esse adipisicing quis et culpa voluptate ad ad
-							ullamco. Lorem reprehenderit ut ipsum amet proident
-							amet aliqua non veniam aute minim ullamco labore.
-							Consectetur sint ut laborum eu culpa id cillum magna
-							tempor anim. Amet tempor sunt consectetur nulla
-							excepteur irure anim qui sit et mollit veniam
-							pariatur amet. Fugiat non quis aute dolor Lorem
-							aliqua minim consectetur ipsum adipisicing id qui
-							ipsum id. Aliqua id minim est enim aute duis ad
-							officia mollit commodo. Ea esse labore est dolore ad
-							eiusmod ad labore enim sit irure sint magna labore.
-							Ut occaecat minim do commodo Lorem adipisicing esse
-							minim commodo cupidatat. Aliquip reprehenderit irure
-							aute voluptate mollit sunt adipisicing quis ut.
-							Occaecat sint incididunt Lorem commodo id incididunt
-							eu enim laborum proident reprehenderit consectetur
-							sint cillum. Ad incididunt irure ea fugiat amet
-							mollit pariatur non proident officia proident
-							adipisicing aliquip sit. Exercitation reprehenderit
-							pariatur excepteur proident aliquip adipisicing
-							officia. Nulla nulla elit ea aliqua exercitation et.
-							Irure id Lorem incididunt minim aliqua ut ea aute
-							fugiat aliquip. Voluptate in Lorem deserunt laboris
-							incididunt sunt nisi mollit cupidatat. Cupidatat
-							occaecat cillum dolore reprehenderit consequat
-							pariatur dolor sit minim ea tempor. Mollit culpa
-							tempor anim consequat consectetur excepteur laborum.
-							Ullamco fugiat occaecat in do sunt ex eu labore
-							irure nostrud sint. Irure amet qui dolore
-							reprehenderit id minim labore elit Lorem culpa et.
-							Aliqua labore tempor nostrud qui sit et labore
-							eiusmod nisi officia pariatur proident Lorem
-							nostrud. Incididunt commodo proident reprehenderit
-							incididunt incididunt. Ex magna ullamco occaecat id
-							proident nulla labore ut occaecat nisi sit est.
-							Proident ea do voluptate cupidatat proident
-							adipisicing id ad adipisicing reprehenderit irure.
-							Sint reprehenderit dolore nostrud esse eiusmod qui
-							mollit commodo tempor enim commodo ea aliqua. Magna
-							ex non irure incididunt sunt labore nisi ullamco
-							quis minim. Anim adipisicing ullamco consequat
-							laboris non excepteur. Tempor dolore adipisicing
-							labore elit reprehenderit id voluptate. Laboris
-							pariatur est id ipsum officia sunt fugiat in eiusmod
-							dolore duis voluptate non. Eu irure nulla nulla
-							cupidatat mollit sunt aliqua ea. Occaecat do qui
-							dolor veniam. Occaecat ut anim irure ipsum magna
-							aute excepteur sint aliquip excepteur excepteur. Ex
-							id irure incididunt ullamco exercitation tempor
-							excepteur aute ut irure reprehenderit labore. Qui
-							esse voluptate occaecat commodo deserunt cupidatat
-							nulla deserunt commodo labore. Consectetur sint amet
-							ex amet aliquip culpa sunt ipsum voluptate pariatur.
-							Ea labore amet minim nisi officia officia anim
-							exercitation irure. Velit et dolore cillum nulla
-							elit consequat enim irure laborum deserunt.
-							Adipisicing qui nostrud dolor proident sunt laborum
-							ut cillum nulla. Eu incididunt occaecat culpa irure
-							pariatur incididunt consectetur anim id. Deserunt
-							qui minim consequat enim reprehenderit duis nostrud
-							minim exercitation amet minim adipisicing. Ipsum
-							Lorem adipisicing et et cillum non aliquip aliquip
-							voluptate aliquip est adipisicing dolore
-							consectetur. Eiusmod quis officia do sint veniam
-							consequat amet.Ipsum voluptate et aliqua quis. Nulla
-							quis nisi non enim ea ex exercitation nulla dolor in
-							elit. Incididunt esse cupidatat nulla veniam
-							reprehenderit. Veniam ad labore elit aliqua velit
-							ipsum. Excepteur tempor non id excepteur minim qui
-							minim. Nisi est tempor in deserunt eu incididunt
-							pariatur tempor fugiat. Ea aliquip Lorem ea mollit
-							nostrud nulla eu aute enim velit fugiat exercitation
-							nostrud pariatur. Exercitation eu et veniam sit.
-							Deserunt magna eu amet est sunt do officia laborum
-							irure elit amet pariatur. Velit nostrud Lorem
-							excepteur enim aliquip velit exercitation culpa
-							ullamco. Est voluptate proident amet quis. Irure
-							amet amet labore nulla fugiat qui anim veniam culpa
-							velit eu aute. Esse sint in non laborum officia ex
-							ad. Consequat velit est magna ipsum id ullamco
-							consectetur mollit amet excepteur. Ullamco aliqua
-							sint magna cillum reprehenderit incididunt esse non
-							commodo proident. Adipisicing incididunt excepteur
-							in cupidatat et nulla eu fugiat aute fugiat. Quis
-							irure cillum excepteur irure deserunt nulla
-							excepteur sit est in velit eu do incididunt. Aliqua
-							nostrud excepteur adipisicing voluptate nulla in ut
-							magna. Irure occaecat ullamco ut pariatur dolore qui
-							aliquip nulla consectetur voluptate minim proident
-							consequat ut. Sint quis proident consectetur mollit
-							qui aute. Aute dolor veniam est ea mollit. Ut dolore
-							mollit magna veniam eu ipsum nulla sunt quis qui.
-							Amet sit sunt mollit in ullamco elit sunt minim elit
-							id culpa irure. Occaecat eu minim laborum id. Velit
-							labore laborum commodo et tempor. Non proident duis
-							proident mollit. Culpa do aute enim et proident esse
-							exercitation elit Lorem velit Lorem sunt do. Ex
-							proident veniam magna consectetur incididunt tempor
-							adipisicing eu aute. Esse cillum sint fugiat officia
-							proident. Eu voluptate in Lorem nisi do fugiat
-							incididunt elit labore ullamco. Exercitation minim
-							tempor ullamco consequat velit ad irure aliquip
-							ullamco eiusmod labore. Veniam sint ullamco sint
-							sint nulla quis aute ex officia aliqua ut. Eu duis
-							laboris deserunt officia ipsum minim esse nostrud et
-							non qui. Mollit veniam non voluptate minim amet qui
-							excepteur. Minim ut duis in id officia ea. Est
-							excepteur magna aliqua sunt. Excepteur id incididunt
-							ex tempor nisi consectetur consectetur magna mollit
-							in do nisi do sint. Consequat do labore excepteur
-							laboris officia anim ex id nisi tempor esse enim
-							ullamco. Dolore aliqua occaecat anim consequat
-							dolore proident labore reprehenderit cillum sint.
-							Deserunt commodo aliqua eiusmod sit ut labore in.
-							Amet enim nulla do laboris ea incididunt eiusmod eu
-							sit nisi. Sunt quis sunt quis irure tempor elit
-							exercitation consequat fugiat. Amet minim ea officia
-							aliqua magna qui non sint fugiat fugiat nisi in
-							reprehenderit magna. Laboris do tempor elit duis do
-							labore voluptate irure dolor enim duis excepteur ea
-							duis. Esse pariatur magna anim mollit pariatur
-							laboris consequat sint cillum exercitation
-							exercitation pariatur. Est aliqua aliqua occaecat
-							exercitation anim mollit minim amet duis et ex.
-							Dolor velit irure
-						</p>
-					</Parallax>
-				</div>
-			</Fragment>
-		</ParallaxProvider>
-	)
-}
 
-export default ClientReview
+const ClientReview = (props) => {
+  return (
+    <ParallaxProvider>
+      <Fragment>
+        <div className={styles.ClientReview}>
+          <Parallax className="parallax" y={[-50, 10]} tagOuter="figure">
+            <h1>Review</h1>
+            <Carousel useKeyboardArrows={true} autoPlay={true} infiniteLoop={true}>
+              <div>
+                <img src={props.Image} alt="" />
+                <p className="legend">
+                  Velit magna qui ea magna adipisicing aute in consectetur. Est
+                  amet in occaecat in. Ullamco sint incididunt velit qui
+                  incididunt duis culpa tempor est cupidatat cillum ullamco
+                  deserunt Lorem. Dolor culpa aliquip irure consectetur sunt
+                  anim in esse. Occaecat nulla ut veniam est quis labore duis.
+                  Consectetur ex qui aliquip proident sunt. Veniam fugiat velit
+                  magna ad ex velit voluptate aliqua exercitation cillum
+                  deserunt dolor. Ipsum nisi qui commodo nisi labore amet
+                  dolore.
+                </p>
+              </div>
+              <div>
+                <img src={props.Image} alt="" />
+                <p className="legend">
+                  Enim mollit et laborum dolor occaecat. Adipisicing veniam
+                  adipisicing dolore ullamco velit sint et excepteur minim
+                  deserunt mollit. Deserunt nostrud veniam nulla ut ex sint sunt
+                  pariatur. Fugiat consectetur amet eiusmod qui amet occaecat.
+                  Minim culpa voluptate nulla duis ullamco cupidatat ex. Ad sit
+                  proident aliqua exercitation.
+                </p>
+              </div>
+              <div>
+                <img src={props.Image} alt="" />
+                <p className="legend">
+                  Proident anim proident ullamco non laborum nostrud eiusmod.
+                  Culpa cupidatat aliquip ipsum non cillum Lorem velit proident
+                  consectetur cillum deserunt minim eiusmod. Voluptate dolore ex
+                  ipsum cupidatat id duis incididunt. Ad ex sint officia commodo
+                  ea aliqua qui tempor tempor et aliquip commodo excepteur
+                  excepteur. Non exercitation cillum tempor do duis anim mollit
+                  incididunt esse nisi enim consequat. Id velit ad id Lorem.
+                  Amet Lorem laboris culpa ea. Officia id id et deserunt sunt
+                  adipisicing sunt dolore cillum. Anim dolor commodo commodo
+                  aute magna ex esse. Officia amet laboris proident Lorem enim
+                  reprehenderit aliqua sunt officia nisi occaecat eiusmod.
+                </p>
+              </div>
+            </Carousel>
+          </Parallax>
+        </div>
+      </Fragment>
+    </ParallaxProvider>
+  );
+};
+
+export default ClientReview;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "../containers/App.css";
 import ClientReview from '../components/ClientReview/ClientReview';
+import Image from "../assets/review.jpg";
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <ClientReview />
+          <ClientReview Image={Image} />
       </div>
     );
   }
