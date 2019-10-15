a7671a4a7b87a9daaed07f6c618ea174961958a8
mahihossain <sayed.muddashir@gmail.com>
2019-10-15 13:12:37 +0600

    Edited the ClientReview componenet

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import Footer from './components/Footer/Footer';
 import Landing from './components/landing/landing';
 import './App.scss';
 import 'normalize.css';
+import ClientReview from './components/ClientReview/ClientReview';
+import OurTeam from './components/OurTeam/OurTeam';
+import Image from './assets/ClientReview/review.jpg';
 
 class App extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<div className="stopOverflow">
+				<div className='stopOverflow'>
 					<Nav />
 					<Landing />
+					{/* <ClientReview Image={Image} />
+					<OurTeam Image={Image} /> */}
 					<Footer />
 				</div>
 			</React.Fragment>
